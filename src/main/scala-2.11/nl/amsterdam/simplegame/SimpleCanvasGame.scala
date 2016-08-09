0691aa98ddeff987bce52b7f2a85145655aff341
SeriousSoftware <vdenberg+github@gmail.com>
2016-08-09 13:11:11 +0200

    feat: Incubation 02

--- src/main/scala-2.11/nl/amsterdam/simplegame/SimpleCanvasGame.scala
+++ src/main/scala-2.11/nl/amsterdam/simplegame/SimpleCanvasGame.scala
@@ -6,19 +6,18 @@ import org.scalajs.dom.ext.KeyCode
 import org.scalajs.dom.html.Canvas
 import org.scalajs.dom.raw.HTMLImageElement
 
+import scala.collection.mutable
 import scala.scalajs.js
 
 case class Position(x: Int, y: Int)
 
-case class Monster(pos: Position)
+class Monster(val pos: Position)
 
-case class Hero(
-                 pos: Position,
-                 speed: Int = 256 // movement in pixels per second
-               )
+class Hero(val pos: Position)
 
 object Hero {
   val size = 32
+  val speed =256
 }
 
 class Image(src: String) {
@@ -33,13 +32,9 @@ class Image(src: String) {
 object SimpleCanvasGame extends js.JSApp {
 
   def main(): Unit = {
-    initGame
-  }
-
-  def initGame(): Unit = {
     // Create the canvas
     val canvas = dom.document.createElement("canvas").asInstanceOf[Canvas]
-    val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
+    val ctx = canvas.getContext("2d") //.asInstanceOf[dom.CanvasRenderingContext2D]
 
     canvas.width = (0.95 * dom.window.innerWidth).toInt
     canvas.height = (0.95 * dom.window.innerHeight).toInt
@@ -49,36 +44,35 @@ object SimpleCanvasGame extends js.JSApp {
     val heroImage = new Image("img/hero.png")
     val monsterImage = new Image("img/monster.png")
 
-    var hero = Hero(Position(0, 0))
-    var monster = Monster(Position(0, 0))
+    var hero = new Hero(Position(0, 0))
+    var monster = new Monster(Position(0, 0))
 
     var monstersCaught = 0
 
     // Handle keyboard controls
-    import scala.collection.mutable.HashMap
-    val keysDown = HashMap[Int, Boolean]()
+    val keysDown = mutable.HashMap[Int, Boolean]()
 
     dom.window.addEventListener("keydown", (e: dom.KeyboardEvent) => {
       keysDown += e.keyCode -> true
-    }, false)
+    }, useCapture = false)
 
     dom.window.addEventListener("keyup", (e: dom.KeyboardEvent) => {
       keysDown -= e.keyCode
-    }, false)
+    }, useCapture = false)
 
     // Reset the game when the player catches a monster
     def reset() = {
-      hero = hero.copy(pos = Position(canvas.width / 2, canvas.height / 2))
+      hero = new Hero(Position(canvas.width / 2, canvas.height / 2))
 
       // Throw the monster somewhere on the screen randomly
-      monster = Monster(Position(
+      monster = new Monster(Position(
         Hero.size + (Math.random() * (canvas.width - 64)).toInt,
         Hero.size + (Math.random() * (canvas.height - 64)).toInt))
     }
 
     // Update game objects
     def update(modifier: Double) {
-      val modif = (hero.speed * modifier).toInt
+      val modif = (Hero.speed * modifier).toInt
       var Position(x, y) = hero.pos
       if (keysDown.contains(KeyCode.Left)) x -= modif
       if (keysDown.contains(KeyCode.Right)) x += modif
@@ -86,9 +80,7 @@ object SimpleCanvasGame extends js.JSApp {
       if (keysDown.contains(KeyCode.Down)) y += modif
 
       val newPos = Position(x, y)
-      if (isValidPosition(newPos, canvas)) {
-        hero = hero.copy(pos = newPos)
-      }
+      if (isValidPosition(newPos, canvas)) hero = new Hero(newPos)
 
       // Are they touching?
       if (areTouching(hero.pos, monster.pos)) {
@@ -99,15 +91,9 @@ object SimpleCanvasGame extends js.JSApp {
 
     // Draw everything
     def render() {
-      if (bgImage.isReady) {
-        ctx.drawImage(bgImage.element, 0, 0, canvas.width, canvas.height)
-      }
-      if (heroImage.isReady) {
-        ctx.drawImage(heroImage.element, hero.pos.x, hero.pos.y)
-      }
-      if (monsterImage.isReady) {
-        ctx.drawImage(monsterImage.element, monster.pos.x, monster.pos.y)
-      }
+      if (bgImage.isReady) ctx.drawImage(bgImage.element, 0, 0, canvas.width, canvas.height)
+      if (heroImage.isReady) ctx.drawImage(heroImage.element, hero.pos.x, hero.pos.y)
+      if (monsterImage.isReady) ctx.drawImage(monsterImage.element, monster.pos.x, monster.pos.y)
 
       // Score
       ctx.fillStyle = "rgb(250, 250, 250)"
@@ -117,6 +103,8 @@ object SimpleCanvasGame extends js.JSApp {
       ctx.fillText("Goblins caught: " + monstersCaught, 32, 32)
     }
 
+
+    // TODO Make this reactive
     var prev = js.Date.now()
     // The main game loop
     val gameLoop = () => {
